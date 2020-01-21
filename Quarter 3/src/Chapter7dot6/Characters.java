489ae0079ec2e4ad7d3184e4d9d68351da2a455b
Zander Dietz <zander.dietz@hilliardschools.org>
2020-01-21 09:22:32 -0500

    Create Characters.java

---
+++ Quarter 3/src/Chapter7dot6/Characters.java
@@ -0,0 +1,11 @@
+package Chapter7dot6;
+import java.io.*;
+public class Characters {
+
+	public static void main(String[] args) throws IOException{
+		File file = new File("\"C:\\Users\\871265\\Desktop\\Characters.txt\"");
+		FileWriter writer = new FileWriter(file);
+		//String[][] str = new String[6][2]; {{"Mickey", "Mouse"}, {"Donald", "Duck"}, {"Goofy", "Goofy"}, {"Minne", "Mouse"}, {"Daisy", "Duck"}, {"Max", "Goofy"}}
+	}
+
+}

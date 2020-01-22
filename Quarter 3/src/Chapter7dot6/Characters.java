ec2c87e2ae428c6adf4c0ffb9629d23bbae69bf8
Zander Dietz <zander.dietz@hilliardschools.org>
2020-01-22 09:15:38 -0500

    finish characters

--- Quarter 3/src/Chapter7dot6/Characters.java
+++ Quarter 3/src/Chapter7dot6/Characters.java
@@ -3,9 +3,28 @@ import java.io.*;
 public class Characters {
 
 	public static void main(String[] args) throws IOException{
-		File file = new File("\"C:\\Users\\871265\\Desktop\\Characters.txt\"");
+		File file = new File("C:\\Users\\871265\\Desktop\\Characters.txt");
 		FileWriter writer = new FileWriter(file);
-		//String[][] str = new String[6][2]; {{"Mickey", "Mouse"}, {"Donald", "Duck"}, {"Goofy", "Goofy"}, {"Minne", "Mouse"}, {"Daisy", "Duck"}, {"Max", "Goofy"}}
+		String[][] str = new String[][] {{"Mickey", "Mouse"}, {"Donald", "Duck"}, {"Goofy", "Goofy"}, {"Minne", "Mouse"}, {"Daisy", "Duck"}, {"Max", "Goofy"}};
+		final int ROWS = 2;
+		final int COLS = 6;
+		writer.write("First and Last: \r\n");
+		for(int i = 0; i < COLS; i++) {
+			writer.write("Row " + i + ": ");
+			for(int j = 0; j < ROWS; j ++) {
+				
+				writer.write(str[i][j] + " ");
+			}
+			writer.write("\r\n");
+		}
+		writer.write("\r\n");
+		writer.write("Last names only: \r\n");
+		for(int i = 0; i < 6; i ++) {
+			writer.write("Row " + i + ": ");
+			writer.write(str[i][1] + "\r\n");
+		}
+		writer.flush();
+		writer.close();
 	}
 
 }
